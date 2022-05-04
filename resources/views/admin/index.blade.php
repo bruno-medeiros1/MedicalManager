5028c58da02e3cb4e1254f9f1daa58c462495c55
Bruno Medeiros <66907632+bruno-medeiros1@users.noreply.github.com>
2022-05-04 17:29:50 +0100

    update admin pages

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -27,7 +27,7 @@
                     <div class="card-body">
                         <h5 class="card-title">Unidades de Saúde</h5>
                         <p class="card-text">Aqui poderás desde criar, editar, apagar e visualizar todas as unidades de saúde existentes.</p>
-                        <a href="#" class="btn btn-primary">Configurar</a>
+                        <a href="/MedicalManager/public/admin/unidades/index" class="btn btn-primary">Configurar</a>
                     </div>
                 </div>
             </div>
@@ -37,7 +37,7 @@
                     <div class="card-body">
                         <h5 class="card-title">Pessoal Médico</h5>
                         <p class="card-text">Aqui poderás desde criar, editar, apagar e visualizar todo o pessoal médico existente.</p>
-                        <a href="#" class="btn btn-primary">Configurar</a>
+                        <a href="/MedicalManager/public/admin/pessoal/index" class="btn btn-primary">Configurar</a>
                     </div>
                 </div>
             </div>
@@ -48,7 +48,7 @@
                     <div class="card-body">
                         <h5 class="card-title">Tipo de Pessoal Médico</h5>
                         <p class="card-text">Aqui poderás desde criar, editar, apagar e visualizar todo os tipos de pessoal médico existentes.</p>
-                        <a href="#" class="btn btn-primary">Configurar</a>
+                        <a href="/MedicalManager/public/admin/tipo/index" class="btn btn-primary">Configurar</a>
                     </div>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,27 @@ Route::get('/admin/especialidades/index', function () {
     return view('/includes/error');
 });
 
+Route::get('/admin/unidades/index', function () {
+    if (Auth::check()) {
+        return view('/admin/unidades/index');
+    }
+    return view('/includes/error');
+});
+
+Route::get('/admin/pessoal/index', function () {
+    if (Auth::check()) {
+        return view('/admin/pessoal/index');
+    }
+    return view('/includes/error');
+});
+
+Route::get('/admin/tipo/index', function () {
+    if (Auth::check()) {
+        return view('/admin/tipo/index');
+    }
+    return view('/includes/error');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
