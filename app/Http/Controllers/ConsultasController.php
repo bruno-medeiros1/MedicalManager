279ea68109f0e559450f01f65341cba2c0898c22
Bruno Medeiros <66907632+bruno-medeiros1@users.noreply.github.com>
2022-05-22 23:50:02 +0100

    CRUD validations
    Added auth to controller to validate calls
    Added seeds for consults

--- app/Http/Controllers/ConsultasController.php
+++ app/Http/Controllers/ConsultasController.php
@@ -6,6 +6,8 @@ use App\Models\Consultas;
 use App\Models\User;
 use Illuminate\Http\Request;
 use DateTime;
+use Illuminate\Validation\Rule;
+use Auth;
 
 
 class ConsultasController extends Controller
@@ -14,108 +16,143 @@ class ConsultasController extends Controller
 
     public function add_consult_form()
     {
-        if( \View::exists('consultas.create') ){
+        if (Auth::check()) {
 
-            return view('consultas.create');
+            if( \View::exists('consultas.create') ){
+
+                return view('consultas.create');
+            }
         }
+        return view('/includes/error');
     }
 
 
     public function submit_consult_data(Request $request)
     {
+        if (Auth::check()) {
 
-        $rules = [
-            'name' => ['required','unique:consultas','max:20'],
-            'description' => ['required'],
-            'date' => ['required'],
-        ];
+            $rules = [
+                'name' => ['required','unique:consultas','max:20'],
+                'description' => ['required'],
+                'date' => ['required'],
+            ];
 
-        $errorMessage = [
-            'required' => 'Este campo é obrigatório',
-            'unique' => 'Esse nome já existe. Insira outro por favor.',
-            'max' => 'O nome não pode exceder dos 20 carateres'
-        ];
+            $errorMessage = [
+                'required' => 'Este campo é obrigatório',
+                'unique' => 'Esse nome já existe. Insira outro por favor.',
+                'max' => 'O nome não pode exceder dos 20 carateres'
+            ];
 
-        $this->validate($request, $rules, $errorMessage);
+            $this->validate($request, $rules, $errorMessage);
 
-        $create = Consultas::create([
-            'name' => $request->name,
-            'description' => strtolower($request->description),
-            'date' => $request->date
-        ]);
+            $create = Consultas::create([
+                'name' => $request->name,
+                'description' => strtolower($request->description),
+                'date' => $request->date
+            ]);
 
-        if($create){
-            $this->meesage('message','Consulta adicionada com sucesso!');
-            return redirect('/consulta/index');
+            if($create){
+                $this->meesage('message','Consulta adicionada com sucesso!');
+                return redirect('/consulta/index');
+            }
+            return redirect()->back();
         }
-        return redirect()->back();
+        return view('/includes/error');
 
     }
 
 
     public function fetch_all_consults()
     {
-        $consultas = Consultas::toBase()->get();
-        return view('consultas.index',compact('consultas'));
+        if (Auth::check()) {
+
+            $consultas = Consultas::toBase()->get();
+            return view('consultas.index',compact('consultas'));
+        }
+        return view('/includes/error');
     }
 
 
     public function edit_consult_form(Consultas $consulta)
     {
-        //  create date time object
-        $date = new DateTime($consulta->date);
+        if (Auth::check()) {
 
-        //  sets the date into the correct format to use in html datetime-local input component
-        $consulta->date = $date->format('Y-m-d\TH:i');
+            //  create date time object
+            $date = new DateTime($consulta->date);
 
-        //  returns the view
-        return view('consultas.edit',compact('consulta'));
+            //  sets the date into the correct format to use in html datetime-local input component
+            $consulta->date = $date->format('Y-m-d\TH:i');
+
+            //  returns the view
+            return view('consultas.edit',compact('consulta'));
+        }
+        return view('/includes/error');
+
     }
 
 
     public function edit_consult_form_submit(Request $request, Consultas $consulta)
     {
-        $rules = [
-            'name' => ['required','unique:consultas','max:20'],
-            'description' => ['required'],
-            'date' => ['required'],
-        ];
+        if (Auth::check()) {
 
-        $errorMessage = [
-            'required' => 'Este campo é obrigatório',
-            'unique' => 'Esse nome já existe. Insira outro por favor.',
-            'max' => 'O nome não pode exceder dos 20 carateres'
-        ];
+            /*Checks if that name exists in db and if so ignore the current id while editing for not displaying error*/
+            $rules = [
+                'name' => ['required', Rule::unique('consultas')->ignore($consulta->id,'id'),'max:20'],
+                'description' => ['required'],
+                'date' => ['required'],
+            ];
 
-        $this->validate($request, $rules, $errorMessage);
+            $errorMessage = [
+                'required' => 'Este campo é obrigatório',
+                'unique' => 'Esse nome já existe. Insira outro por favor.',
+                'max' => 'O nome não pode exceder dos 20 carateres'
+            ];
 
-        $update = $consulta->update([
-            'name' => $request->name,
-            'description' => strtolower($request->description),
-            'date' => $request->date
-        ]);
+            $this->validate($request, $rules, $errorMessage);
 
-        if($update){
-            $this->meesage('message','A consulta foi atualizada com sucesso!');
-            return redirect('/consulta/index');
+            $update = $consulta->update([
+                'name' => $request->name,
+                'description' => strtolower($request->description),
+                'date' => $request->date
+            ]);
+
+            if($update){
+                $this->meesage('message','A consulta foi atualizada com sucesso!');
+                return redirect('/consulta/index');
+            }
+            return redirect()->back();
         }
-        return redirect()->back();
+        return view('/includes/error');
+
     }
 
 
     public function view_single_consult(Consultas $consulta)
     {
-        return view('consultas.view',compact('consulta'));
+        if (Auth::check()) {
+
+            //  create date time object
+            $date = new DateTime($consulta->date);
+
+            //  sets the date into the correct format to use in html datetime-local input component
+            $consulta->date = $date->format('Y-m-d\TH:i');
+
+            return view('consultas.view',compact('consulta'));
+        }
+        return view('/includes/error');
     }
 
 
     public function delete_consult(Consultas $consulta)
     {
-        echo 'chegou!';
+        if (Auth::check()) {
 
-        $consulta->delete();
-        $this->meesage('message','Consulta apagada com sucesso!');
-        return redirect()->back();
+            $consulta->delete();
+            $this->meesage('message','Consulta apagada com sucesso!');
+            return redirect()->back();
+        }
+        return view('/includes/error');
+
     }
 
 
---
+++ database/seeds/ConsultasSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Consultas;
+
+class ConsultasSeeder extends Seeder
+{
+    /*  Seeding our Consult table*/
+    public function run()
+    {
+        $consulta = new Consultas();
+        $consulta->name = 'Consulta Geral';
+        $consulta->description = 'Consulta geral para o Sr. João marcada no dia 25 de Junho às 9h da manhã.';
+        $consulta->date = new DateTime('2022-06-25 09:00');
+        $consulta->save();
+
+        $consulta = new Consultas();
+        $consulta->name = 'Consulta Ortopedia';
+        $consulta->description = 'Consulta de ortopedia para o Sr. Manuel marcada no dia 15 de Junho às 9h da manhã.';
+        $consulta->date = new DateTime('2022-06-15 09:00');
+        $consulta->save();
+
+        $consulta = new Consultas();
+        $consulta->name = 'Consulta Cardiologia';
+        $consulta->description = 'Consulta de cardiologia para a Sra. Joana marcada no dia 5 de Julho às 9:30h da manhã.';
+        $consulta->date = new DateTime('2022-07-5 09:30');
+        $consulta->save();
+
+        $consulta = new Consultas();
+        $consulta->name = 'Consulta Dermatologia';
+        $consulta->description = 'Consulta de dermatologia para a Sra. Beatriz marcada no dia 29 de Agosto às 10:30h da manhã.';
+        $consulta->date = new DateTime('2022-08-29 10:30');
+        $consulta->save();
+
+        $consulta = new Consultas();
+        $consulta->name = 'Consulta Endocrinologia';
+        $consulta->description = 'Consulta de endocrinologia para a Sr. Bruno marcada no dia 10 de Setembro às 10:30h da manhã.';
+        $consulta->date = new DateTime('2022-09-10 10:30');
+        $consulta->save();
+    }
+}
